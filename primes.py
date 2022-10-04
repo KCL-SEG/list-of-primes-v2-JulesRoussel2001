ddf7e0b9c0c77ba8fc76c573a993291135a26d63
Jules Roussel <k20074057@kcl.ac.uk>
2022-10-04 16:42:01 +0100

    All tests passed for primes v2

--- primes.py
+++ primes.py
@@ -2,5 +2,17 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes <= 0:
+        raise ValueError(f"{number_of_primes} should have been a positive value.")
+    else:
+        list = []
+        num = 2
+        while len(list) < number_of_primes:
+            count = 0
+            for i in range(1,num):
+                if num % i == 0:
+                    count += 1
+            if count == 1:
+                list.append(num)
+            num += 1
+        return list
